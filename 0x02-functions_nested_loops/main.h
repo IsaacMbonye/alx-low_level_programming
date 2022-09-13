2253920142471ea2b5fff3aac7982b478bf5b1fa
Mbonye Isaac <mbonyeisac@gmail.com>
2022-09-13 16:05:38 +0000

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-/*int _putchar(char c);*/
+#ifndef MAIN_H_
+#define MAIN__H_
+/*
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+*/
 #include <unistd.h>
 
-#include <unistd.h>
-
-/**
+/*
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
@@ -17,4 +28,26 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-#endif 
+
+
+
+#endif  /* MAIN_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
