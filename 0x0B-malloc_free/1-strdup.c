a6020e78cd131b5d2b877a10df203521e2736756
IsaacMbonye <mbonyeisac@gmail.com>
2022-10-04 09:13:07 +0000

    imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,17 +9,21 @@
 char *_strdup(char *str)
 {
 	char *a;
-	long unsigned int i;
+	int i, c;
 	
 	if (str == NULL)
 		return (NULL);
 
-	a = malloc (sizeof *str);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc (i * sizeof(*a) + 1);
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < sizeof(a); i++)
-		a[i] = str[i];
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
 
 	return (a);
 }
