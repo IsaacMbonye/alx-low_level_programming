f6da51c5db00dc6a0a56c7ae5f2f9f0501de7f16
IsaacMbonye <mbonyeisac@gmail.com>
2022-10-05 16:15:11 +0000

    this is it

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **strtow - function returns a pointer to an array of strings(words)
+ */
+char **strtow(char *str)
+{
+	char *a;
+	unsigned int i;
+	str[] = "Hello World";
+
+	if (str == NULL)
+		return (NULL);
+	if (str == "")
+		return (NULL);
+
+	a = malloc(i * sizeof(*a));
+
+	for (i = 0; i < size; i++)
+	{
+		if (str == NULL)
+			return (NULL);
+	     
+	       char * token = strtok(str, " ");
+	 }
+	
+	 return (a);
+}
+        	
+
