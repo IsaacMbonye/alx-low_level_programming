1667fc7d668985c711233e3bb0421c8637d47bda
IsaacMbonye <mbonyeisac@gmail.com>
2022-10-07 07:19:00 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,37 +11,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *a;
-	int i, j, p, d;
-	
-	for (i = 0; (i < s1[i]) != '\0'; i++)
-		;
-	for (j =0; (j < s2[j]) != '\0'; j++)
-		;
-	
-	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)));
-	if (a == NULL)
+	char *p;
+	int strlen1, i, c;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	strlen1 = (unsigned int)strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for(i = 0, c = 0; (i < strlen1) + n; i++)
 	{
-		a = "";
-
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
 	}
+	p[i] = '\0';
 
-	n = sizeof(s2[0]);
-	for (p = 0, d = 0; p < (i + j + 1); p++)
-	{
-		if (n < strlen(s2))
-		{
-			a[p] = s1[p] + n;
-		}
-		if (n >= strlen(s2))
-		{
-			a[p] = s1[p] + s2[d++];
-		}
-	
-	}
-
-	free(a);
-	return (a);
+	return (p);
 		
+}
+
+/**
+ * _strnlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
 
