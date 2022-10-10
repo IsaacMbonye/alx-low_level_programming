3f40793bfbdddf18f06ecffa325bbfc021548007
IsaacMbonye <mbonyeisac@gmail.com>
2022-10-10 18:01:48 +0000

    the dog is the only one thing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,14 @@
  * A function that initialize a variable of type struct dog
  */
 
-void int_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog *));
-	if (d == NULL)
-		return (1);
-	free(d);
-	return (0);
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	
 }
 
